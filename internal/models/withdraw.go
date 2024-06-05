1b91f9f9dca4544efb651c7fa6a31af2c8e8eb84
Dmitriy Loginov <dmitriy@loginoff.space>
2024-06-05 17:31:55 +0300

    Лицензия проекта

--- internal/models/withdraw.go
+++ internal/models/withdraw.go
@@ -1,3 +1,10 @@
+// This file is part of the gophermart-bonus project
+//
+// © 2024 Dmitriy Loginov
+//
+// Licensed under the MIT License. See the LICENSE.md file in the project root for more information.
+//
+// https://github.com/FlutterDizaster/gophermart-bonus
 package models
 
 //easyjson:json
